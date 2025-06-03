3823eff4364be8273b08a2f76922ec0d80456b7d
BrunoDev@777 <brugui2703@gmail.com>
2025-06-03 14:37:33 -0300

    Pequenos ajustes de instalacao

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { CustomerModule } from './modules/customer/customer.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { Module } from '@nestjs/common';
-import { ProductModule } from './modules/product/product.module';v
+import { ProductModule } from './modules/product/product.module';
 // import { APP_GUARD } from '@nestjs/core';
 // import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 // import { PaymentModule } from './payment/payment.module';
--- server/src/modules/product/product.module.ts
+++ server/src/modules/product/product.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductController } from './controllers/product.controller';
 import { ProductService } from './services/product.service';
 import { ProductRepository } from './repositories/product.repository';
+import { PrismaService } from 'src/common/services/prisma.service';
 
 @Module({
   imports: [],
   controllers: [ProductController],
-  providers: [ProductService, ProductRepository],
+  providers: [ProductService, ProductRepository, PrismaService],
 })
 export class ProductModule {}
