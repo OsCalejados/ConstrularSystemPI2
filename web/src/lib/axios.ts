3895e1fd0e92ca1d4c9e2a5cd88add71e2b4ee74
Pedro Grigorio <pedroogrigorio@gmail.com>
2025-06-21 19:53:14 -0300

    fix to send header tokens

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001/',
+  withCredentials: true,
 })
 
 api.interceptors.request.use(
